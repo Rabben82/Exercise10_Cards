d849b9edf8b7562b7bc6c799f63205b293ae9c89
Rabben82 <89846052+Rabben82@users.noreply.github.com>
2023-10-25 15:18:18 +0200

    added showing values

--- cards.js
+++ cards.js
@@ -3,6 +3,7 @@ let url = 'https://deckofcardsapi.com/api/deck/new/draw/?count=1';
 
 const inputField = document.querySelector('#input-card');
 const output = document.querySelector('#output-card');
+const outputText = document.querySelector('#output-text');
 const button = document.querySelector('#button-draw');
 
 button.addEventListener('click', function (){
@@ -31,8 +32,11 @@ function GetDeck(){
     .then(data =>{
         const cardImage = document.createElement('img');
         cardImage.setAttribute('src', data.cards[0].image);
-
+        
         output.innerHTML = '';
+        outputText.innerHTML = '';
+        
+        outputText.innerHTML = `${data.cards[0].suit}\t${data.cards[0].value}`
         output.appendChild(cardImage);
     })
     .catch(error => console.error('Error fetching card:', error))
